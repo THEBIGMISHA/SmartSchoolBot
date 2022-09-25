a836056823500d5b8b1d85475bdc91ef31a89e60
Michael Nikolaev <esp12p@gmail.com>
2022-09-26 00:51:36 +0300

    22.9.26

--- bot/bot.py
+++ bot/bot.py
@@ -5,7 +5,7 @@ import logger
 import datacenter			
 import os
 import time				
-from telebot import types 	
+from telebot import types
 #####SETUP
 logger.log('WS','BOT','WORKING')
 bot = telebot.TeleBot(config.token)
@@ -18,19 +18,24 @@ def START(message):
 	bot.send_message(message.chat.id,f'<b>Привет, {str(message.from_user.first_name)}!\n Я бот помощьник 8Б класса\n Все мои функии в кнопке "меню"</b>', parse_mode='html')
 	if message.chat.type == 'private':
 		bot.send_message(message.chat.id,'<b>↓↓↓↓↓</b>',parse_mode='html')
+	bot.delete_message(message.chat.id, message.message_id)
 @bot.message_handler(commands=['settings'])
 def SETTINGS(message):
 	logger.log('W','BOT',f'settings: NAME: {str(message.from_user.first_name)} USER-ID: {str(message.from_user.id)} CHAT-ID: {str(message.chat.id)}')
 	bot.send_message(message.chat.id,'<b>СКОРО</b>',parse_mode='html')
+	bot.delete_message(message.chat.id, message.message_id)
 @bot.message_handler(commands=['qr'])
 def QR(message):
 	logger.log('W','BOT',f'qr: NAME: {str(message.from_user.first_name)} USER-ID: {str(message.from_user.id)} CHAT-ID: {str(message.chat.id)}')
 	photo=open(f"{config.media}Pqr.jpg", "rb")
 	bot.send_photo(message.chat.id,photo)
-@bot.message_handler(commands=['ping' , 'test'])
+	time.sleep(20)
+	bot.delete_message(message.chat.id, message.message_id)
+@bot.message_handler(commands=['ping'])
 def PING(message):
 	logger.log('W','BOT',f'ping, test: NAME: {str(message.from_user.first_name)} USER-ID: {str(message.from_user.id)} CHAT-ID: {str(message.chat.id)}')
 	bot.send_message(message.chat.id, '<b>БОТ РАБОТАЕТ!</b>', parse_mode='html')
+	bot.delete_message(message.chat.id, message.message_id)
 @bot.message_handler(commands=['admin'])
 def ADMIN(message):
 	photo=open(f"{config.media}Padmin.jpg", "rb")
@@ -46,9 +51,15 @@ def ADMIN(message):
 	markup.add(buttonD)
 	logger.log('W','BOT',f'admin: NAME: {str(message.from_user.first_name)} USER-ID: {str(message.from_user.id)} CHAT-ID: {str(message.chat.id)}')
 	bot.send_message(message.chat.id,'<b>Cот-сети главного админа:</b>',parse_mode='html', reply_markup=markup)
+	bot.delete_message(message.chat.id, message.message_id)
 @bot.message_handler(commands=['bug_report'])
-def ADMIN(message):
-	bot.send_message(message.chat.id,'<b>Скоро:</b>',parse_mode='html')
+def BUG_REPORT(m):
+	if m.text == '/bug_report':
+		bot.send_message(m.chat.id,f'<b>{m.from_user.first_name}, чтобы отправить баг репорт, нужно написать сообщение так\n \n/bug_report Расписание устарело!</b>',parse_mode='html')
+	else: 
+		bot.send_message(m.chat.id,f'<b>{m.from_user.first_name}, спасибо за репорт бага!</b>',parse_mode='html')
+		bot.send_message(1641679319,f'<b>{m.from_user.first_name}, отправил репорт бага!\n \n {m.text}</b>',parse_mode='html')
+		logger.log('WS','BOT',f'BUG_REPORT: NAME: {str(m.from_user.first_name)} USER-ID: {str(m.from_user.id)} CHAT-ID: {str(m.chat.id)}\nMESSAGE:\n{m.text}')
 @bot.message_handler(commands=['schedule'])
 def SCHEDULE(message):
 	markup = types.InlineKeyboardMarkup()
@@ -64,6 +75,7 @@ def SCHEDULE(message):
 	markup.add(buttonE)
 	logger.log('W','BOT',f'schedule: NAME: {str(message.from_user.first_name)} USER-ID: {str(message.from_user.id)} CHAT-ID: {str(message.chat.id)}')
 	bot.send_message(message.chat.id,'<b>Выбери день недели:</b>',parse_mode='html', reply_markup=markup)
+	bot.delete_message(message.chat.id, message.message_id)
 @bot.callback_query_handler(func=lambda call: True)
 def CALL(call):
 	if call.message:
@@ -109,84 +121,79 @@ def CALL(call):
 			logger.log('W','BOT',f'schedule/5: CHAT-ID: {str(call.message.chat.id)}')
 			bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,text=datacenter.schedule(5), parse_mode='html', reply_markup=markup)
 		#ADMIN_PANEL
-		elif call.data == 'logger':
-			markup = types.InlineKeyboardMarkup()
-			buttonA = types.InlineKeyboardButton(text='Запросить логи', callback_data='logger/save')
-			buttonB = types.InlineKeyboardButton(text='Удалить логи', callback_data='logger/hm')
-			markup.add(buttonA)
-			markup.add(buttonB)
-			logger.log('W','BOT',f'logger: CHAT-ID: {str(call.message.chat.id)}')
-			bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,text='Логирование', parse_mode='html', reply_markup=markup)
-		elif call.data == 'stop/hm':
-			markup = types.InlineKeyboardMarkup()
-			buttonA = types.InlineKeyboardButton(text='Да, точно!', callback_data='stop/yes')
-			buttonB = types.InlineKeyboardButton(text='Нет', callback_data='bot_del_mess')
-			buttonC = types.InlineKeyboardButton(text='Нет', callback_data='bot_del_mess')
-			buttonD = types.InlineKeyboardButton(text='Нет', callback_data='bot_del_mess')
-			buttonE = types.InlineKeyboardButton(text='Нет', callback_data='bot_del_mess')
-			buttonF = types.InlineKeyboardButton(text='Нет', callback_data='bot_del_mess')
-			markup.add(buttonB)
-			markup.add(buttonC)
-			markup.add(buttonD)
-			markup.add(buttonA)
-			markup.add(buttonE)
-			markup.add(buttonF)
-			logger.log('W','BOT',f'stop/hm: CHAT-ID: {str(call.message.chat.id)}')
-			bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,text='ВЫ ТОЧНО ХОТИТЕ ОСТАНОВИТЬ БОТА?', parse_mode='html', reply_markup=markup)
-		elif call.data == 'logger/hm':
-			markup = types.InlineKeyboardMarkup()
-			buttonA = types.InlineKeyboardButton(text='Да, точно!', callback_data='logger/delete')
-			buttonB = types.InlineKeyboardButton(text='Нет', callback_data='bot_del_mess')
-			buttonC = types.InlineKeyboardButton(text='Нет', callback_data='bot_del_mess')
-			buttonD = types.InlineKeyboardButton(text='Нет', callback_data='bot_del_mess')
-			buttonE = types.InlineKeyboardButton(text='Нет', callback_data='bot_del_mess')
-			buttonF = types.InlineKeyboardButton(text='Нет', callback_data='bot_del_mess')
-			markup.add(buttonB)
-			markup.add(buttonC)
-			markup.add(buttonD)
-			markup.add(buttonA)
-			markup.add(buttonE)
-			markup.add(buttonF)
-			logger.log('W','BOT',f'logger/hm: CHAT-ID: {str(call.message.chat.id)}')
-			bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,text='ВЫ ТОЧНО ХОТИТЕ УДАЛИТЬ ЛОГИ?', parse_mode='html', reply_markup=markup)
-		elif call.data == 'bot_del_mess':
-			bot.answer_callback_query(callback_query_id=call.id, show_alert=False,text="ОТМЕНЕНО")
-			bot.delete_message(call.message.chat.id, call.message.message_id)
-			pass
-		elif call.data == 'logger/delete':
-			bot.answer_callback_query(callback_query_id=call.id, show_alert=False,text="ЛОГИ УДАЛЕНЫ")
-			open(config.logfile, 'w').close()
-			logger.log('W','BOT',f'logger/delete: CHAT-ID: {str(call.message.chat.id)}')
-			bot.delete_message(call.message.chat.id, call.message.message_id)
-		elif call.data == 'stop/yes':
-			logger.log('W','BOT',f'stop/bot: CHAT-ID: {str(call.message.chat.id)}')
-			bot.answer_callback_query(callback_query_id=call.id, show_alert=False,text="БОТ ОСТАНОВЛЕН")
-			bot.delete_message(call.message.chat.id, call.message.message_id)
-			AMOGUSISSUS()
-		elif call.data == 'logger/save':
-			bot.answer_callback_query(callback_query_id=call.id, show_alert=False,text="ЛОГ ФАЙЛ ГОТОВ")
-			bot.delete_message(call.message.chat.id, call.message.message_id)
-			bot.send_document(call.message.chat.id, open(config.logfile, 'rb'))
-			logger.log('W','BOT',f'logger/save: CHAT-ID: {str(call.message.chat.id)}')
-	else:
-		bot.answer_callback_query(callback_query_id=call.id, show_alert=False,text="ERROR")
+		elif call.from_user.id in config.adminid:
+			if call.data == 'logger':
+				markup = types.InlineKeyboardMarkup()
+				buttonA = types.InlineKeyboardButton(text='Запросить логи', callback_data='logger/save')
+				buttonB = types.InlineKeyboardButton(text='Удалить логи', callback_data='logger/hm')
+				markup.add(buttonA)
+				markup.add(buttonB)
+				logger.log('W','BOT',f'logger: CHAT-ID: {str(call.message.chat.id)}')
+				bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,text='Логирование', parse_mode='html', reply_markup=markup)
+			elif call.data == 'stop/hm':
+				markup = types.InlineKeyboardMarkup()
+				buttonA = types.InlineKeyboardButton(text='Да, точно!', callback_data='stop/yes')
+				buttonB = types.InlineKeyboardButton(text='Нет', callback_data='bot_del_mess')
+				buttonC = types.InlineKeyboardButton(text='Нет', callback_data='bot_del_mess')
+				buttonD = types.InlineKeyboardButton(text='Нет', callback_data='bot_del_mess')
+				buttonE = types.InlineKeyboardButton(text='Нет', callback_data='bot_del_mess')
+				buttonF = types.InlineKeyboardButton(text='Нет', callback_data='bot_del_mess')
+				markup.add(buttonB)
+				markup.add(buttonC)
+				markup.add(buttonD)
+				markup.add(buttonA)
+				markup.add(buttonE)
+				markup.add(buttonF)
+				logger.log('W','BOT',f'stop/hm: CHAT-ID: {str(call.message.chat.id)}')
+				bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,text='ВЫ ТОЧНО ХОТИТЕ ОСТАНОВИТЬ БОТА?', parse_mode='html', reply_markup=markup)
+			elif call.data == 'logger/hm':
+				markup = types.InlineKeyboardMarkup()
+				buttonA = types.InlineKeyboardButton(text='Да, точно!', callback_data='logger/delete')
+				buttonB = types.InlineKeyboardButton(text='Нет', callback_data='bot_del_mess')
+				buttonC = types.InlineKeyboardButton(text='Нет', callback_data='bot_del_mess')
+				buttonD = types.InlineKeyboardButton(text='Нет', callback_data='bot_del_mess')
+				buttonE = types.InlineKeyboardButton(text='Нет', callback_data='bot_del_mess')
+				buttonF = types.InlineKeyboardButton(text='Нет', callback_data='bot_del_mess')
+				markup.add(buttonB)
+				markup.add(buttonC)
+				markup.add(buttonD)
+				markup.add(buttonA)
+				markup.add(buttonE)
+				markup.add(buttonF)
+				logger.log('W','BOT',f'logger/hm: CHAT-ID: {str(call.message.chat.id)}')
+				bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,text='ВЫ ТОЧНО ХОТИТЕ УДАЛИТЬ ЛОГИ?', parse_mode='html', reply_markup=markup)
+			elif call.data == 'bot_del_mess':
+				bot.answer_callback_query(callback_query_id=call.id, show_alert=False,text="ОТМЕНЕНО")
+				bot.delete_message(call.message.chat.id, call.message.message_id)
+				pass
+			elif call.data == 'logger/delete':
+				bot.answer_callback_query(callback_query_id=call.id, show_alert=False,text="ЛОГИ УДАЛЕНЫ")
+				open(config.logfile, 'w').close()
+				logger.log('W','BOT',f'logger/delete: CHAT-ID: {str(call.message.chat.id)}')
+				bot.delete_message(call.message.chat.id, call.message.message_id)
+			elif call.data == 'stop/yes':
+				logger.log('W','BOT',f'stop/bot: CHAT-ID: {str(call.message.chat.id)}')
+				bot.answer_callback_query(callback_query_id=call.id, show_alert=False,text="БОТ ОСТАНОВЛЕН")
+				bot.delete_message(call.message.chat.id, call.message.message_id)
+				AMOGUSISSUS()
+			elif call.data == 'logger/save':
+				bot.answer_callback_query(callback_query_id=call.id, show_alert=False,text="ЛОГ ФАЙЛ ГОТОВ")
+				bot.delete_message(call.message.chat.id, call.message.message_id)
+				bot.send_document(call.message.chat.id, open(config.logfile, 'rb'))
+				logger.log('W','BOT',f'logger/save: CHAT-ID: {str(call.message.chat.id)}')
+		else:
+			bot.answer_callback_query(callback_query_id=call.id, show_alert=False,text="ERROR")
 @bot.message_handler(commands=['admin_panel'])
 def ADMIN_PANEL(message):
-	if message.chat.type == 'private':
-		if message.chat.id in config.adminid:
-			markup = types.InlineKeyboardMarkup()
-			buttonA = types.InlineKeyboardButton(text='Логирование', callback_data='logger')
-			buttonB = types.InlineKeyboardButton(text='Остановить бота', callback_data='stop/hm')
-			markup.add(buttonA)
-			markup.add(buttonB)
-			bot.send_message(message.chat.id,'<b>Админка</b>', parse_mode='html',reply_markup=markup)
-			logger.log('WS','BOT',f'admin_panel: NAME: {str(message.from_user.first_name)} USER-ID: {str(message.from_user.id)} CHAT-ID: {str(message.chat.id)}')
-		else:
-			bot.send_message(message.chat.id,f'<b>{str(message.from_user.first_name)}, вы НЕ админ\nваш id: {str(message.from_user.id)}</b>', parse_mode='html')
+	if message.from_user.id in config.adminid:
+		markup = types.InlineKeyboardMarkup()
+		buttonA = types.InlineKeyboardButton(text='Логирование', callback_data='logger')
+		buttonB = types.InlineKeyboardButton(text='Остановить бота', callback_data='stop/hm')
+		markup.add(buttonA)
+		markup.add(buttonB)
+		bot.send_message(message.chat.id,'<b>Админка</b>', parse_mode='html',reply_markup=markup)
+		logger.log('WS','BOT',f'admin_panel: NAME: {str(message.from_user.first_name)} USER-ID: {str(message.from_user.id)} CHAT-ID: {str(message.chat.id)}')
 	else:
-		if message.chat.id in config.adminid:
-			bot.send_message(message.chat.id,f'<b>{str(message.from_user.first_name)}, вы админ\nНо вы не можете это отправить в группе</b>', parse_mode='html')
-		else: 
-			bot.send_message(message.chat.id,f'<b>{str(message.from_user.first_name)}, вы НЕ админ\nваш id: {str(message.from_user.id)}</b>', parse_mode='html')
-
+		bot.send_message(message.chat.id,f'<b>{str(message.from_user.first_name)}, вы НЕ админ\nваш id: {str(message.from_user.id)}</b>', parse_mode='html')
+	bot.delete_message(message.chat.id, message.message_id)	
 bot.polling(none_stop=True)	
--- bot/config.py
+++ bot/config.py
@@ -1,6 +1,4 @@
 token	='5617195281:AAFwTXLUF7oN24CXz2kZbUKoGyP_a8DYd_o'
-adminid	=[1641679319, 1710722564]
+adminid	=[1641679319]
 logfile ='/home/michael/git/SmartSchoolBot/bot/log.ini'
 media = '/home/michael/git/SmartSchoolBot/bot/media/'
-generaladminid = 1641679319
-generaladmin = '@THEBIGMISHA'
